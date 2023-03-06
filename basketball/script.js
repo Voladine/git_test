bc1a539e8149b483d705b30385bc4baa55ed4e3e
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-03-05 22:17:10 -0500

    Scrimba basketball solo project

---
+++ basketball/script.js
@@ -0,0 +1,47 @@
+let homeboard = document.getElementById("homescore")
+let awayboard = document.getElementById("awayscore")
+let hcount = 0
+let acount = 0
+
+function onehp() {
+    hcount += 1
+    homeboard.textContent = hcount
+}
+
+function twohp() {
+    hcount += 2
+    homeboard.textContent = hcount
+}
+
+function threehp() {
+    hcount += 3
+    homeboard.textContent = hcount
+}
+
+function oneap() {
+    acount += 1
+    awayboard.textContent = acount
+}
+
+function twoap() {
+    acount += 2
+    awayboard.textContent = acount
+}
+
+function threeap() {
+    acount += 3
+    awayboard.textContent = acount
+}
+
+function hreset() {
+    homeboard.textContent = 0
+    hcount = 0
+}
+
+function areset() {
+    awayboard.textContent = 0
+    acount = 0
+}
+
+
+ 
